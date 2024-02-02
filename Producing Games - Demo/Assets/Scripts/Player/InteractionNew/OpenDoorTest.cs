214b0edde442ebc8c0607238cb8e7c3dac6fc5d5
Nathan <nat0tpsn@gmail.com>
2024-02-02 14:19:01 +0000

    Shake on door open
    just to demo camera shake on event

--- Producing Games - Demo/Assets/Scripts/Player/InteractionNew/OpenDoorTest.cs
+++ Producing Games - Demo/Assets/Scripts/Player/InteractionNew/OpenDoorTest.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class OpenDoorTest : InteractableTemplate
 {
+    public CameraShake cameraShake;
     public override void Interact()
     {
         Debug.Log("*** Door code is running ***");
         gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(0,1000,0), Time.deltaTime);
+        StartCoroutine(cameraShake.CamShake(1.0f, .2f));
     }
 }
