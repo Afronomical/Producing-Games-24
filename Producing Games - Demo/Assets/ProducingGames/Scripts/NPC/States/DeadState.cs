ff8daee9dcbbeb36ca7941804a36e3d092f41f2e
MattB166 <matthewbrake2001@hotmail.co.uk>
2024-02-04 22:49:40 +0000

    velocity checks dead state

--- Producing Games - Demo/Assets/ProducingGames/Scripts/NPC/States/DeadState.cs
+++ Producing Games - Demo/Assets/ProducingGames/Scripts/NPC/States/DeadState.cs
@@ -7,6 +7,9 @@ public class DeadState : StateBaseClass
     public override void UpdateLogic()
     {
         Debug.Log("NPC DEAD");
+        character.rb.velocity = Vector3.zero;
+        character.agent.velocity = Vector3.zero;
+        character.agent.ResetPath(); 
         transform.Rotate(0, 0 ,- 90);
     }
 }
