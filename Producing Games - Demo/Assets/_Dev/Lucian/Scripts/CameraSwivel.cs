7097fe6b7833ec1e99871a65f9a0998f4308437a
Lucian Dusciac <DUSCIACLUCIAN@YAHOO.COM>
2024-02-05 16:27:13 +0000

    more comments...

--- Producing Games - Demo/Assets/_Dev/Lucian/Scripts/CameraSwivel.cs
+++ Producing Games - Demo/Assets/_Dev/Lucian/Scripts/CameraSwivel.cs
@@ -9,7 +9,7 @@ public class CameraSwivel : MonoBehaviour
     public Transform pivot;
 
     [Header("Camera Rotation Settings")]
-    public bool canTurn;
+    public bool canTurn;//variable for deciding if the camera is static or not
     public float waitTime;
     public float spinTime;
     public float turnSpeed;
@@ -30,13 +30,14 @@ public class CameraSwivel : MonoBehaviour
         {
             if(elapsedTime <= 0)
             {
-                //SwitchDirection();
+                //stop rotation if time runs out
                 internalTurnSpeed = 0;
                 Invoke("SwitchDirection", waitTime);
                 elapsedTime = spinTime + waitTime;
             }
             else
             {
+                //carry on rotating while timer is going
                 elapsedTime -= Time.deltaTime;
                 transform.RotateAround(pivot.position, Vector3.up, switchDirection ? -internalTurnSpeed : internalTurnSpeed);
             }
@@ -47,7 +48,7 @@ public class CameraSwivel : MonoBehaviour
 
     private void SwitchDirection()
     {
-        
+        //speaks for itself...
         switchDirection = !switchDirection;
         internalTurnSpeed = turnSpeed;
     }
