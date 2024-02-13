7ab0a632298e4fcc15ebceda81633cfdd3158d34
Alex Izak <izak_alex@yahoo.com>
2024-02-13 16:44:59 +0000

    Started Implementing Cardiograph
    Working on editing the particle system through code

---
+++ Producing Games - Demo/Assets/_Dev/Alex/Scripts/HRM.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HRM : MonoBehaviour
+{
+    private ParticleSystem hRMonitor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hRMonitor = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //hRMonitor.Emit()
+
+        //Switch from normal vitals to heart attack vitals
+
+    }
+
+    //Function for 
+}
