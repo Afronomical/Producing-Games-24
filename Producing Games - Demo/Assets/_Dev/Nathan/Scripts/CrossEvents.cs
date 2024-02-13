9c3051b9502b947a79426f9e9849119e01b00cc9
Nathan <nat0tpsn@gmail.com>
2024-02-13 13:35:18 +0000

    Horror Showcase scene
    Nathan Test scene will now be used as a showcase for the events and effects that the horror team create until these are implemented into the game

---
+++ Producing Games - Demo/Assets/_Dev/Nathan/Scripts/CrossEvents.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossEvents : MonoBehaviour
+{
+    public GameObject Cross;
+    public Rigidbody rb;
+    public float rotationSpeed;
+    private float rotTime;
+    public float throwingForce;
+    bool isRotating;
+    bool isTriggered;
+
+    private void Start()
+    {
+        rb.useGravity = false;
+        isTriggered = false;
+    }
+
+    private void Update()
+    {
+        invertedCross();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isTriggered == false)
+        {
+            int eventType = UnityEngine.Random.Range(0, 2);
+            if (eventType == 0)
+            {
+                isRotating = true;
+                isTriggered = true;
+            }
+            else if (eventType == 1)
+            {
+                FallingCross();
+                isTriggered = true;
+            }
+            else //(eventType == 2) 
+            {
+                Debug.Log("No Cross Event");
+                isTriggered = true;
+            }
+        }
+
+    }
+
+
+    void FallingCross()
+    {
+        rb.useGravity = true;
+        rb.AddForce(Vector3.back * throwingForce, ForceMode.Impulse);
+        //rb.isKinematic = true;
+    }
+
+    void invertedCross()
+    {
+        if (isRotating)
+        {
+            rotTime += (Time.deltaTime * rotationSpeed);
+            Cross.transform.localEulerAngles = new Vector3(0, 0, Mathf.Lerp(0, 180, rotTime));
+        }
+    }
+}
