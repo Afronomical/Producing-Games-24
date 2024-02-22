f69706619a65a6e8fdcbc2eedc9994dbd1587f37
Nathan <nat0tpsn@gmail.com>
2024-02-22 20:16:37 +0000

    updates to shake on spot script

--- Producing Games - Demo/Assets/_Dev/Nathan/Scripts/ShakeOnSpot.cs
+++ Producing Games - Demo/Assets/_Dev/Nathan/Scripts/ShakeOnSpot.cs
@@ -1,3 +1,5 @@
+using JetBrains.Annotations;
+using NUnit.Framework.Constraints;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,10 +12,9 @@ public class ShakeOnSpot : MonoBehaviour
     public Rigidbody[] Items;
     [Space]
     [Header("Shake Settings")]
-    public float shakeForce;
     public float maxRotAngle = 45f;
     public float shakeTime;
-
+    public float shakeForce;
     void Start()
     {
         isTriggered = false;
@@ -21,37 +22,32 @@ public class ShakeOnSpot : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.CompareTag("Player") && !isTriggered)
         {
             StartCoroutine(ShakingOnSpot());
             isTriggered = true;
         }
-
     }
 
     IEnumerator ShakingOnSpot()
     {
         float startRotation = 0f;
         float delta = 0f;
-
-       
+        
         //chose a random item in list
         int randIndex = Random.Range(0, Items.Length);
-
-        Items[randIndex].useGravity = false;
 
         // Shake the item using max rotation angle and define time with shakeTime variable
         while (delta <= shakeTime)
         {
-            switch (startRotation < maxRotAngle)
+            switch (startRotation <= maxRotAngle)
             {
-                case true:
-                    Items[randIndex].AddRelativeTorque(Vector3.up * shakeForce, ForceMode.Impulse);
+                case true:                    
+                    Items[randIndex].AddRelativeTorque(Vector3.right * shakeForce, ForceMode.Impulse);
                     startRotation += Mathf.Abs(shakeForce);
                     break;
                 case false:
-                    Items[randIndex].AddRelativeTorque(Vector3.up * shakeForce, ForceMode.Impulse);
+                    Items[randIndex].AddRelativeTorque(Vector3.left * shakeForce, ForceMode.Impulse);
                     startRotation -= Mathf.Abs(shakeForce);
                     break;
             }
@@ -59,8 +55,6 @@ public class ShakeOnSpot : MonoBehaviour
             yield return null;
         }
 
-        yield return new WaitForSeconds(1);
-        Items[randIndex].useGravity = true;
-
+        yield return null;
     }
 }
