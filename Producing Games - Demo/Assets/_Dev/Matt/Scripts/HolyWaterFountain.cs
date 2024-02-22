6d2c891b0aeb06f38ab5aea546776e6fccefa0ff
MattB166 <matthewbrake2001@hotmail.co.uk>
2024-02-22 13:16:17 +0000

    cannot drop items if they are not exorcisable objects

---
+++ Producing Games - Demo/Assets/_Dev/Matt/Scripts/HolyWaterFountain.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolyWaterFountain : InteractableTemplate
+{
+    public override void Interact()
+    {
+       //collect water from the fountain if it has any 
+    }
+}
