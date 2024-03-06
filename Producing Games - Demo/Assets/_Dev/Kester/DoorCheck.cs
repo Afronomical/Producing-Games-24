d678e7005dde359c1338517a810f4a2a3500be9f
Wolf-Sleuth <kesterdersonperson@gmail.com>
2024-03-06 22:28:20 +0000

    DoorCheck and HFoodEdit
    Minor error fixed in HFood SO, and the door check has been continued.

--- Producing Games - Demo/Assets/_Dev/Kester/DoorCheck.cs
+++ Producing Games - Demo/Assets/_Dev/Kester/DoorCheck.cs
@@ -6,21 +6,56 @@ public class DoorCheck : MonoBehaviour
 {
     private GameObject Door;
     private GameObject FakeDoor;
+    private GameObject TaskManager;
+    public int stage = 0;
 
     private void Start()
     {
         Door = transform.Find("Study Door").gameObject;
         FakeDoor = transform.Find("Fake Door").gameObject;
         Door.SetActive(false);
+        TaskManager = GameObject.Find("TaskManager");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (DiegeticUIManager.Instance.hasChecklist && DiegeticUIManager.Instance.hasDemonBook && DiegeticUIManager.Instance.hasPager)
+        if (DiegeticUIManager.Instance.hasChecklist && DiegeticUIManager.Instance.hasDemonBook && DiegeticUIManager.Instance.hasPager && stage == 0)
         {
             Door.SetActive(true);
             FakeDoor.SetActive(false);
+            stage++;
+        }
+
+        if(stage == 1 && GameManager.Instance.currentTime > 0.07f && GameManager.Instance.currentHour == 1)
+        {
+            Door.SetActive(false);
+            FakeDoor.SetActive(true);
+            stage++;
+        }
+
+        if (stage == 2) {
+            Task[] CurrentTasks = TaskManager.GetComponents<Task>();
+            for (int i = 0; i < CurrentTasks.Length; i++)
+            {
+                if (!CurrentTasks[i].taskCompleted)
+                {
+                    break;
+                }
+                if(i == CurrentTasks.Length - 1)
+                {
+                    Door.SetActive(true);
+                    FakeDoor.SetActive(false);
+                    stage++;
+                }
+            }
+        }
+        
+        if (stage == 3)
+        {
+            Door.SetActive(false);
+            FakeDoor.SetActive(true);
+            stage++;
         }
     }
 }
