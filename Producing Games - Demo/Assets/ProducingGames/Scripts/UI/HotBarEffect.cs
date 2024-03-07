4d1ab5f0680df689017ad4de76bfbd4d88321142
HugoM101 <5macdh24@solent.ac.uk>
2024-03-07 16:31:44 +0000

    Accessibility Settings Update
    adding all relevant functionality to new manager

---
+++ Producing Games - Demo/Assets/ProducingGames/Scripts/UI/HotBarEffect.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HotBarEffect : MonoBehaviour
+{
+    
+    [SerializeField] private AccessibilitySettingsManager AccessibilitySettings;
+
+    private void Start()
+    {
+
+        
+        AccessibilitySettings.inventoryScaleSlider.onValueChanged.AddListener(SetinventoryPanelScale);
+        AccessibilitySettings.originalInventorySize = AccessibilitySettings.inventoryPanel.localScale;
+        AccessibilitySettings.originalInventoryAspect = AccessibilitySettings.inventoryPanel.localScale.x / AccessibilitySettings.inventoryPanel.localScale.y;
+        AccessibilitySettings.inventoryPanelCanvasGroup = AccessibilitySettings.inventoryPanel.GetComponent<CanvasGroup>();
+        if (AccessibilitySettings.inventoryPanelCanvasGroup == null)
+        {
+           
+            AccessibilitySettings.inventoryPanelCanvasGroup = AccessibilitySettings.inventoryPanel.gameObject.AddComponent<CanvasGroup>();
+        }
+
+      
+        AccessibilitySettings.inventoryPanelCanvasGroup.alpha = 0.0f;
+
+        InventoryHotbar.instance.OnItemPickedUp += StartSizeEffect;
+        InventoryHotbar.instance.OnItemSelected += StartSizeEffect;
+    }
+
+    private void OnDestroy()
+    {
+        InventoryHotbar.instance.OnItemPickedUp -= StartSizeEffect;
+        InventoryHotbar.instance.OnItemSelected -= StartSizeEffect;
+    }
+
+    private void SetinventoryPanelScale(float inventoryScaleValue)
+    {
+        float newValx = inventoryScaleValue * AccessibilitySettings.originalInventoryAspect;
+        float newValy = inventoryScaleValue;
+
+        AccessibilitySettings.inventoryPanel.localScale = new Vector3(newValx, newValy, 1f);
+        AccessibilitySettings.originalInventorySize = AccessibilitySettings.inventoryPanel.localScale;
+    }
+
+    
+
+    private IEnumerator SizeEffectCoroutine()
+    {
+        float elapsedTimeForInvEffect = 0f;
+        Vector3 targetInventorySize = AccessibilitySettings.originalInventorySize * AccessibilitySettings.effectScaleMultiplier;
+
+        while (elapsedTimeForInvEffect < 1f)
+        {
+            AccessibilitySettings.inventoryPanel.localScale = Vector3.Lerp(AccessibilitySettings.originalInventorySize, targetInventorySize, elapsedTimeForInvEffect);
+            AccessibilitySettings.inventoryPanelCanvasGroup.alpha = Mathf.Lerp(0.0f, 1f, elapsedTimeForInvEffect); //Increase alpha
+            elapsedTimeForInvEffect += Time.deltaTime * AccessibilitySettings.increaseEffectSpeed;
+            yield return null;
+        }
+
+        AccessibilitySettings.inventoryPanel.localScale = targetInventorySize;
+
+        float timer = 0f;
+        while (timer < AccessibilitySettings.effectWaitDuration)
+        {
+            if (Input.mouseScrollDelta.y != 0)
+            {
+                timer = 0f;
+            }
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        elapsedTimeForInvEffect = 0f;
+        while (elapsedTimeForInvEffect < 1f)
+        {
+            AccessibilitySettings.inventoryPanel.localScale = Vector3.Lerp(targetInventorySize, AccessibilitySettings.originalInventorySize, elapsedTimeForInvEffect);
+            AccessibilitySettings.inventoryPanelCanvasGroup.alpha = Mathf.Lerp(1f, 0.0f, elapsedTimeForInvEffect); //Decrease alpha
+            elapsedTimeForInvEffect += Time.deltaTime * AccessibilitySettings.decreaseEffectSpeed;
+            yield return null;
+        }
+
+        AccessibilitySettings.inventoryPanel.localScale = AccessibilitySettings.originalInventorySize;
+        AccessibilitySettings.inventoryPanelCanvasGroup.alpha = 0.0f; 
+
+        AccessibilitySettings.isInventoryIncreasing = false;
+    }
+
+    private void StartSizeEffect()
+    {
+        if (!AccessibilitySettings.isInventoryIncreasing)
+        {
+            AccessibilitySettings.isInventoryIncreasing = true;
+            StartCoroutine(SizeEffectCoroutine());
+        }
+    }
+}
--- Producing Games - Demo/Assets/_Dev/Liam/Scripts/Settings Managers/AccessibilitySettingsManager.cs
+++ Producing Games - Demo/Assets/_Dev/Liam/Scripts/Settings Managers/AccessibilitySettingsManager.cs
@@ -1,22 +1,45 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition; 
+using System.Collections.Generic;
+using System;
+
+
 
 public class AccessibilitySettingsManager : MonoBehaviour
 {
-    [Header("Accessibility Elements")]
-    [SerializeField] private GameObject settingsPanel;
-    [SerializeField] private GameObject pauseMenu;
-    [SerializeField] private Slider inventoryScaleSlider;
-
-    [HideInInspector] public bool isInventoryIncreasing = false;
-    [HideInInspector] public CanvasGroup inventoryPanelCanvasGroup;
-    [HideInInspector] public float originalInventoryAspect;
-
+    [Header("Inventory Elements")]
+    public RectTransform inventoryPanel;
     public float increaseEffectSpeed = 3f;
     public float decreaseEffectSpeed = 3f;
     public float effectWaitDuration = 1.5f;
     public float effectScaleMultiplier = 1.2f;
+    public Slider inventoryScaleSlider;
+    [HideInInspector] public Vector3 originalInventorySize;
+    [HideInInspector] public bool isInventoryIncreasing = false;
+    [HideInInspector] public CanvasGroup inventoryPanelCanvasGroup;
+    [HideInInspector] public float originalInventoryAspect;
+
+    [Header("Colour Blindness Elements")]
+    public Volume colorBlindnessVolume;
+    public TMP_Dropdown colorBlindnessDropdown;
+    private ChannelMixer channelMixerSettings;
+
+    public enum ColorBlindnessMode
+    {
+        Normal,
+        Protanopia,
+        Deuteranopia,
+        Tritanopia
+    }
+
+    
+    [Header("Motion Blur Elements")]
+    public Volume volume;
+    public Slider motionBlurSlider;
+    private MotionBlur motionBlurVolume; 
 
     private static AccessibilitySettingsManager _instance;
 
@@ -47,19 +70,183 @@ public class AccessibilitySettingsManager : MonoBehaviour
 
     void Start()
     {
-        if (inventoryScaleSlider == null)
+
+        /*
+        ************************************
+        **********Colour Blindness**********
+        ************************************
+        */
+        //setting up the dropdown
+        colorBlindnessDropdown.ClearOptions();
+        colorBlindnessDropdown.AddOptions(new List<string> { "Normal", "Protanopia", "Deuteranopia", "Tritanopia" });
+
+        //Get Channel Mixer settings
+        if (colorBlindnessVolume.profile.TryGet(out channelMixerSettings))
         {
-            Debug.LogError("UI reference for inventory scale slider is not assigned in the Unity Editor.");
-            return;
+           
+            colorBlindnessDropdown.onValueChanged.AddListener(OnDropdownValueChanged);
         }
 
-        inventoryScaleSlider.onValueChanged.AddListener(OnInventoryPanelScaleChanged);
-        originalInventoryAspect = inventoryScaleSlider.value;
+
+        /*
+        ************************************
+        ************Motion Blur*************
+        ************************************
+        */
+
+        if (volume.profile.TryGet(out motionBlurVolume))
+        {
+            UpdateMotionBlurIntensity();
+        }
+
+        motionBlurSlider.onValueChanged.AddListener(OnMotionBlurSliderChanged);
+        
     }
 
-    public void OnInventoryPanelScaleChanged(float value)
+   
+   
+   
+    /*
+        ************************************************
+        **************    FUNCTIONS    *****************
+        ************************************************
+    */
+
+
+
+    /*
+        ************************************
+        **********Colour Blindness**********
+        ************************************
+    */
+#region ColourBlindess
+    private void OnDropdownValueChanged(int value)
     {
-        // Add accessibility settings related functionality here
+        ColorBlindnessMode mode = (ColorBlindnessMode)value;
+        SetColorBlindnessMode(mode);
     }
+
+    private void SetColorBlindnessMode(ColorBlindnessMode mode)
+    {
+        switch (mode)
+        {
+            //call function depending on which mode selected
+            case ColorBlindnessMode.Normal:
+                ResetChannelMixerSettings();
+                break;
+
+            case ColorBlindnessMode.Protanopia:
+                SetProtanopiaChannelMixerSettings();
+                break;
+
+            case ColorBlindnessMode.Deuteranopia:
+                SetDeuteranopiaChannelMixerSettings();
+                break;
+
+            case ColorBlindnessMode.Tritanopia:
+                SetTritanopiaChannelMixerSettings();
+                break;
+        }
+    }
+
+
+    //based of minimal research. needs more looking into to set correct colours. 
+    
+    //functions to swap channels.
+    private void ResetChannelMixerSettings()
+    {
+        //Reset to normal
+        channelMixerSettings.redOutRedIn.value = 100f;
+        channelMixerSettings.greenOutRedIn.value = 0f;
+        channelMixerSettings.blueOutRedIn.value = 0f;
+
+        channelMixerSettings.redOutGreenIn.value = 0f;
+        channelMixerSettings.greenOutGreenIn.value = 100f;
+        channelMixerSettings.blueOutGreenIn.value = 0f;
+
+        channelMixerSettings.redOutBlueIn.value = 0f;
+        channelMixerSettings.greenOutBlueIn.value = 0f;
+        channelMixerSettings.blueOutBlueIn.value = 100f;
+    }
+
+
+    private void SetProtanopiaChannelMixerSettings()
+    {
+
+        channelMixerSettings.redOutRedIn.value = 0f;
+        channelMixerSettings.greenOutRedIn.value = 100f;
+        channelMixerSettings.blueOutRedIn.value = 0f;
+
+        channelMixerSettings.redOutGreenIn.value = 100f;
+        channelMixerSettings.greenOutGreenIn.value = 0f;
+        channelMixerSettings.blueOutGreenIn.value = 0f;
+
+        channelMixerSettings.redOutBlueIn.value = 0f;
+        channelMixerSettings.greenOutBlueIn.value = 0f;
+        channelMixerSettings.blueOutBlueIn.value = 100f;
+    }
+
+    private void SetDeuteranopiaChannelMixerSettings()
+    { 
+        channelMixerSettings.redOutRedIn.value = 0f;
+        channelMixerSettings.greenOutRedIn.value = 0f;
+        channelMixerSettings.blueOutRedIn.value = 100f;
+
+        channelMixerSettings.redOutGreenIn.value = 100f;
+        channelMixerSettings.greenOutGreenIn.value = 0f;
+        channelMixerSettings.blueOutGreenIn.value = 0f;
+
+        channelMixerSettings.redOutBlueIn.value = 0f;
+        channelMixerSettings.greenOutBlueIn.value = 100f;
+        channelMixerSettings.blueOutBlueIn.value = 0f;
+    }
+
+    private void SetTritanopiaChannelMixerSettings()
+    {
+        channelMixerSettings.redOutRedIn.value = 100f;
+        channelMixerSettings.greenOutRedIn.value = 0f;
+        channelMixerSettings.blueOutRedIn.value = 0f;
+
+        channelMixerSettings.redOutGreenIn.value = 0f;
+        channelMixerSettings.greenOutGreenIn.value = 0f;
+        channelMixerSettings.blueOutGreenIn.value = 100f;
+
+        channelMixerSettings.redOutBlueIn.value = 0f;
+        channelMixerSettings.greenOutBlueIn.value = 100f;
+        channelMixerSettings.blueOutBlueIn.value = 0f;
+    }
+
+#endregion
+
+
+
+    /*
+        ************************************
+        ************Motion Blur*************
+        ************************************
+    */
+#region MotionBlur
+
+private void OnMotionBlurSliderChanged(float value)
+    {
+        if (motionBlurVolume != null)
+        {
+            //update the motion blur setting in volume with value from slider
+            motionBlurVolume.intensity.value = value;
+        }
+    }
+
+    
+
+    //set slider value to motionblur intensity when called in start
+    private void UpdateMotionBlurIntensity()
+    {
+        if (motionBlurVolume != null)
+        {
+            motionBlurSlider.value = motionBlurVolume.intensity.value;
+        }
+    }
+
+#endregion 
 }
 
