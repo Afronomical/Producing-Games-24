ab0386130a184eef6766aacb714f6a645d3dad3a
Alex Izak <izak_alex@yahoo.com>
2024-03-08 18:36:13 +0000

    Setup the Static effect on the cardiogram

--- Producing Games - Demo/Assets/_Dev/Alex/Scripts/HRM Toggle.cs
+++ Producing Games - Demo/Assets/_Dev/Alex/Scripts/HRM Toggle.cs
@@ -6,7 +6,7 @@ public class HRMToggle : InteractableTemplate
 {
     public override void Interact()
     {
-        if(HRM.instance.gameObject.activeSelf) HRM.instance.TurnOff();
+        if(!HRM.instance.staticEffect.activeSelf) HRM.instance.TurnOff();
         else HRM.instance.TurnOn();
     }
 }
