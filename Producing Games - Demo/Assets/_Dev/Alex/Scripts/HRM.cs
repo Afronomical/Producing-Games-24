5befafe38f131dcf45acd3876bcb6f9b22eff70b
Alex Izak <izak_alex@yahoo.com>
2024-03-13 12:45:33 +0000

    Changed HRM from instance to normal script
    Due to it being an instance, only 1 monitor would display static, this is now fixed

--- Producing Games - Demo/Assets/_Dev/Alex/Scripts/HRM.cs
+++ Producing Games - Demo/Assets/_Dev/Alex/Scripts/HRM.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using TMPro;
 using UnityEngine;
 
 public class HRM : MonoBehaviour
 {
-    public static HRM instance;
+    //public static HRM instance;
     public TMP_Text hRateText;
-    private int bPM;
+    private int bPM = 70;
     public GameObject staticEffect;
     //public AnimationCurve heartRate;
+    private float timer = 0;
+    public float timerInterval = 24;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(instance == null)
-            instance = this;
+        //if(instance == null)
+        //    instance = this;
 
         //heartRate = GetComponent<AnimationCurve>();
 
@@ -29,15 +32,21 @@ public class HRM : MonoBehaviour
         //heartRate.AddKey(1, 0);
 
         //Display HR Graph
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer++;
         //Display heart rate with the curve
-        bPM = UnityEngine.Random.Range(60, 100);
-        hRateText.text = "HR  " + bPM + " (bpm)";
+        if(timer >= timerInterval)
+        {
+            timer = 0;
+            if (UnityEngine.Random.Range(60, 100) > bPM)
+                bPM += 1;
+            else bPM -= 1;
+            hRateText.text = "HR  " + bPM + " (bpm)";
+        }
 
         //Switch from normal vitals to heart attack vitals
 
@@ -61,6 +70,7 @@ public class HRM : MonoBehaviour
     {
         gameObject.GetComponent<ParticleSystem>().Play();
         staticEffect.SetActive(false);
+        Debug.Log("On!");
     }
 
     public void TurnOff()
@@ -68,5 +78,6 @@ public class HRM : MonoBehaviour
         gameObject.GetComponent<ParticleSystem>().Stop();
         gameObject.GetComponent<ParticleSystem>().Clear();
         staticEffect.SetActive(true);
+        Debug.Log("Off!");
     }
 }
