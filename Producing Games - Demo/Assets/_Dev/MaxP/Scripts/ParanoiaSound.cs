45ecc71fdef68c5843c6d1ace2457688e4a9ba9e
MaxP <max.piper.uk@gmail.com>
2024-03-15 08:44:04 +0000

    working on heart beats pitch effect

--- Producing Games - Demo/Assets/_Dev/MaxP/Scripts/ParanoiaSound.cs
+++ Producing Games - Demo/Assets/_Dev/MaxP/Scripts/ParanoiaSound.cs
@@ -9,19 +9,18 @@ using UnityEngine;
 
 public class ParanoiaSound : MonoBehaviour
 {
+    //public int minChance;
+    //public int maxChance;
+
     public SoundEffect soundTest;
     
     public SoundEffect[] sounds;
     public float distanceMin;
     public float distanceMax;
     public bool shouldPlay = true;
-    public int minChance;
-    public int maxChance;
-    public float cooldownTime;
-    private bool cooldown;
-    private int chance;
+    public float cooldownTimeMin = 10;
+    public float cooldownTimeMax = 30;
     private float mainDistance;
-    private float distanceDelta;
     private GameObject playerObj;
     private float randX;
     private float randZ;
@@ -30,34 +29,33 @@ public class ParanoiaSound : MonoBehaviour
     void Start()
     {
         playerObj = GameManager.Instance.player;
+        if (shouldPlay)
+        {
+            StartCoroutine(Cooldown());
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.M))
         {
-            AudioManager.instance.PlaySound(soundTest, transform);
-        }
-        if (shouldPlay)
-        {
-            ChanceForEvent();
-        }
-
-    }
-
-    void ChanceForEvent()
-    {
-        chance = (int)Random.Range(0, Mathf.Lerp(maxChance, minChance, GameManager.Instance.eventChance / 100)); //(Random.Range(1, 100) <= GameManager.Instance.eventChance)
-        if (chance == 1 && !cooldown)
-        {
-            PlaySoundAtRandLoc();
-            StartCoroutine(Cooldown());
+            //AudioManager.instance.PlaySound(soundTest, transform);
+            PlaySoundAtRandLoc(false);
         }
     }
 
-    void PlaySoundAtRandLoc()
+    //void ChanceForEvent()
+    //{
+    //    chance = (int)Random.Range(0, Mathf.Lerp(maxChance, minChance, GameManager.Instance.eventChance / 100)); //(Random.Range(1, 100) <= GameManager.Instance.eventChance)
+    //    if (chance == 1 && !cooldown)
+    //    {
+    //        PlaySoundAtRandLoc(false);
+    //        StartCoroutine(Cooldown());
+    //    }
+    //}
+
+    void PlaySoundAtRandLoc(bool startCooldown)
     {
         mainDistance = Random.Range(distanceMin, distanceMax);
         randX = mainDistance - Random.Range(0, mainDistance);
@@ -66,13 +64,18 @@ public class ParanoiaSound : MonoBehaviour
         randZ *= Random.Range(0, 2) * 2 - 1;
 
         transform.position = new Vector3(randX, 0, randZ) + playerObj.transform.position;
+
         AudioManager.instance.PlaySound(sounds[Random.Range(0, sounds.Length)], transform);
+
+        if (startCooldown)
+        {
+            StartCoroutine(Cooldown());
+        }
     }
 
     private IEnumerator Cooldown()
     {
-        cooldown = true;
-        yield return new WaitForSeconds(cooldownTime);
-        cooldown = false;
+        yield return new WaitForSeconds(Random.Range(cooldownTimeMin, cooldownTimeMax));
+        PlaySoundAtRandLoc(true);
     }
 }
