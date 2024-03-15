4ad56c753488b7f3fa6840ad97c3dd22affcaf59
MaxP <max.piper.uk@gmail.com>
2024-03-15 13:59:53 +0000

    more heartbeat shit

--- Producing Games - Demo/Assets/_Dev/MaxP/Scripts/HeartBeat.cs
+++ Producing Games - Demo/Assets/_Dev/MaxP/Scripts/HeartBeat.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class HeartBeat : MonoBehaviour
 {
     public SoundEffect heartBeatSound;
-
+    [Range(0.0f, 3f)] public float maxPitch;
+    [Range(0.0f, 3f)] public float minPitch;
+    public float heartFalloffTime = 3;
     private GameObject demonObj;
     private AudioSource newSound;
     private bool isPlaying;
@@ -30,17 +33,31 @@ public class HeartBeat : MonoBehaviour
 
     public void HeartRadius()
     {
-        float pitchTemp;
-        if (!isPlaying)
+        float pitchTemporary;
+        float maxDistance = demonDist();
+
+        while (demonDist() < maxDistance)
         {
             AudioManager.instance.PlaySound(heartBeatSound, transform);
-
+            pitchTemporary = Mathf.Lerp(minPitch, maxPitch, demonDist() / maxDistance);
         }
-        isPlaying = true;
+
     }
 
-    //float demonDist()
-    //{
 
-    //}
+    IEnumerator HeartBeatStop()
+    {
+        yield return new WaitForSeconds(heartFalloffTime);
+        float time = heartFalloffTime / 2;
+        while (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+    }
+
+    float demonDist()
+    {
+        return Vector3.Distance(demonObj.transform.position, transform.position);
+    }
+
 }
