d824de81cce771517787cbf9f298549160ae1d52
AJM-03 <a.moreland2003@gmail.com>
2024-03-17 15:24:14 +0000

    Potential Audio Fix

--- Producing Games - Demo/Assets/ProducingGames/Scripts/Audio/AudioManager.cs
+++ Producing Games - Demo/Assets/ProducingGames/Scripts/Audio/AudioManager.cs
@@ -66,67 +66,70 @@ public class AudioManager : MonoBehaviour
 
     public void PlaySound(SoundEffect effect, Transform effectParent)
     {
-        AudioSource source = musicSource;
-
-        for (int i = 0; i < effectSources.Count; i++)
+        if (effect != null)
         {
-            if (!effectSources[i].isPlaying)
-            {
-                source = effectSources[i];
-                break;
-            }
-        }
+            AudioSource source = musicSource;
 
-        if (source == musicSource)  // If it couldn't find an empty source
-        {
-            AudioSource lowestPrioritySource = effectSources[0];
-            for (int i = 0; i < effectSources.Count; i++)  // Find the active source with the lowest priority
+            for (int i = 0; i < effectSources.Count; i++)
             {
-                if (effectSources[i].priority < lowestPrioritySource.priority)
+                if (!effectSources[i].isPlaying)
                 {
-                    lowestPrioritySource = effectSources[i];
+                    source = effectSources[i];
+                    break;
                 }
             }
 
-            Debug.Log("Sound effect stopped due to not having enough free sources, Source: " + lowestPrioritySource.name + " Priority: " + lowestPrioritySource.priority);
-        }
-
-
-        SetMainSourceSettings(source, effect);
-
-        if (effect.spacialAudio)
-        {
-            source.transform.position = effectParent.position;
-
-            if (effect.followObject)
-                source.transform.parent = effectParent;
-            else
-                source.transform.parent = transform;
-
-            source.spatialBlend = 1;
-            source.maxDistance = effect.travelDistance;
-            source.SetCustomCurve(AudioSourceCurveType.CustomRolloff, distanceFalloff);
-        }
-        else
-        {
-            source.spatialBlend = 0;
-            source.transform.parent = transform;
-        }
-
-        source.Play();
-
-        //Creates an invisible sphere that will be the size of the max distance that the sound can be heard from, any Audio listener scripts within that sphere will be detected and triggered
-        Collider[] col = Physics.OverlapSphere(source.transform.position, source.maxDistance);
-        for (int i = 0; i < col.Length; i++)
-        {
-            if (col[i].TryGetComponent(out AudioListenScript audioListener))
+            if (source == musicSource)  // If it couldn't find an empty source
             {
-                audioListener.canSoundBeHeard = true;
+                AudioSource lowestPrioritySource = effectSources[0];
+                for (int i = 0; i < effectSources.Count; i++)  // Find the active source with the lowest priority
+                {
+                    if (effectSources[i].priority < lowestPrioritySource.priority)
+                    {
+                        lowestPrioritySource = effectSources[i];
+                    }
+                }
 
+                Debug.Log("Sound effect stopped due to not having enough free sources, Source: " + lowestPrioritySource.name + " Priority: " + lowestPrioritySource.priority);
             }
-            else if (col[i].TryGetComponent(out IHear character) && effect.canBeHeardByDemon)
+
+
+            SetMainSourceSettings(source, effect);
+
+            if (effect.spacialAudio)
             {
-                character.ReactToSound(effectParent);
+                source.transform.position = effectParent.position;
+
+                if (effect.followObject)
+                    source.transform.parent = effectParent;
+                else
+                    source.transform.parent = transform;
+
+                source.spatialBlend = 1;
+                source.maxDistance = effect.travelDistance;
+                source.SetCustomCurve(AudioSourceCurveType.CustomRolloff, distanceFalloff);
+            }
+            else
+            {
+                source.spatialBlend = 0;
+                source.transform.parent = transform;
+            }
+
+            source.Play();
+
+            //Creates an invisible sphere that will be the size of the max distance that the sound can be heard from, any Audio listener scripts within that sphere will be detected and triggered
+            Collider[] col = Physics.OverlapSphere(source.transform.position, source.maxDistance);
+            for (int i = 0; i < col.Length; i++)
+            {
+                if (col[i].TryGetComponent(out AudioListenScript audioListener))
+                {
+                    audioListener.canSoundBeHeard = true;
+
+                }
+                else if (col[i].TryGetComponent(out IHear character) && effect.canBeHeardByDemon)
+                {
+                    character.ReactToSound(effectParent);
+                }
             }
         }
     }
