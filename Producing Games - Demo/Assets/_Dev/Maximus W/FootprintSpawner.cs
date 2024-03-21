4941f261aa0703037b5e89789d2fdb213dd94f84
Maximus Wade <98542158+Maxsta2002@users.noreply.github.com>
2024-03-21 21:12:09 +0000

    Update FootprintSpawner.cs

--- Producing Games - Demo/Assets/_Dev/Maximus W/FootprintSpawner.cs
+++ Producing Games - Demo/Assets/_Dev/Maximus W/FootprintSpawner.cs
@@ -1,18 +1,29 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FootprintSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject footprintPrefab;
+    public float spawnInterval = 0.5f;
+    public float footprintLifetime = 5f;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player")) // Adjust the tag as needed
+        {
+            StartCoroutine(SpawnFootprints());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SpawnFootprints()
     {
-        
+        while (true)
+        {
+            Vector3 randomPosition = transform.position + Random.insideUnitSphere * 2f; // Adjust radius as needed
+            Quaternion randomRotation = Quaternion.Euler(0f, Random.Range(0f, 360f), 0f);
+            GameObject footprint = Instantiate(footprintPrefab, randomPosition, randomRotation);
+            yield return new WaitForSeconds(spawnInterval);
+            Destroy(footprint, footprintLifetime);
+        }
     }
 }
