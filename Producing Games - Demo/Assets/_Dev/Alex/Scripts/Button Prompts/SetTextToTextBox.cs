b85d80e9699a2b305a65dcf579fb8b2c6e89ea25
Alex Izak <izak_alex@yahoo.com>
2024-03-26 16:41:26 +0000

    Finished off button prompts

--- Producing Games - Demo/Assets/_Dev/Alex/Scripts/Button Prompts/SetTextToTextBox.cs
+++ Producing Games - Demo/Assets/_Dev/Alex/Scripts/Button Prompts/SetTextToTextBox.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(TMP_Text))]
 public class SetTextToTextBox : MonoBehaviour
@@ -13,6 +14,7 @@ public class SetTextToTextBox : MonoBehaviour
     [Header("Setup for sprites")]
     [SerializeField] private ListOfSpriteAssets listOfSpriteAssets;
     [SerializeField] private DeviceType deviceType;
+    [SerializeField] private InputActions inputAction;
 
     private PlayerControls playerControls;
     private TMP_Text textBox;
@@ -38,9 +40,95 @@ public class SetTextToTextBox : MonoBehaviour
             return;
         }
 
-        textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
-            playerControls.Player.OpenPager.bindings[(int)deviceType],
-            listOfSpriteAssets.spriteAssets[(int)deviceType]);
+        switch (inputAction)
+        {
+            case InputActions.Look:
+                textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
+                    playerControls.Player.Look.bindings[(int)deviceType],
+                    listOfSpriteAssets.spriteAssets[(int)deviceType]);
+                break;
+
+            case InputActions.Movement:
+                textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
+                    playerControls.Player.Movement.bindings[(int)deviceType],
+                    listOfSpriteAssets.spriteAssets[(int)deviceType]);
+                break;
+
+            case InputActions.Jump:
+                textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
+                    playerControls.Player.Jump.bindings[(int)deviceType],
+                    listOfSpriteAssets.spriteAssets[(int)deviceType]);
+                break;
+
+            case InputActions.Sprint:
+                textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
+                    playerControls.Player.Sprint.bindings[(int)deviceType],
+                    listOfSpriteAssets.spriteAssets[(int)deviceType]);
+                break;
+
+            case InputActions.Crouch:
+                textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
+                    playerControls.Player.Crouch.bindings[(int)deviceType],
+                    listOfSpriteAssets.spriteAssets[(int)deviceType]);
+                break;
+
+            case InputActions.Flashlight:
+                textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
+                    playerControls.Player.Flashlight.bindings[(int)deviceType],
+                    listOfSpriteAssets.spriteAssets[(int)deviceType]);
+                break;
+
+            case InputActions.DropItem:
+                textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
+                    playerControls.Player.DropItem.bindings[(int)deviceType],
+                    listOfSpriteAssets.spriteAssets[(int)deviceType]);
+                break;
+
+            case InputActions.PickUp:
+                textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
+                    playerControls.Player.PickUp.bindings[(int)deviceType],
+                    listOfSpriteAssets.spriteAssets[(int)deviceType]);
+                break;
+
+            case InputActions.SeeTasks:
+                textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
+                    playerControls.Player.SeeTasks.bindings[(int)deviceType],
+                    listOfSpriteAssets.spriteAssets[(int)deviceType]);
+                break;
+
+            case InputActions.OpenPager:
+                textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
+                    playerControls.Player.OpenPager.bindings[(int)deviceType],
+                    listOfSpriteAssets.spriteAssets[(int)deviceType]);
+                break;
+
+            case InputActions.ConsumeItem:
+                textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
+                    playerControls.Player.ConsumeItem.bindings[(int)deviceType],
+                    listOfSpriteAssets.spriteAssets[(int)deviceType]);
+                break;
+
+            case InputActions.Scroll:
+                textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
+                    playerControls.Player.Scroll.bindings[(int)deviceType],
+                    listOfSpriteAssets.spriteAssets[(int)deviceType]);
+                break;
+
+            case InputActions.OpenBook:
+                textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
+                playerControls.Player.OpenBook.bindings[(int)deviceType],
+                    listOfSpriteAssets.spriteAssets[(int)deviceType]);
+                break;
+
+            case InputActions.StopInspecting:
+                textBox.text = SwapTextWithSprite.ReadAndReplaceBinding(message,
+                playerControls.Player.StopInspecting.bindings[(int)deviceType],
+                    listOfSpriteAssets.spriteAssets[(int)deviceType]);
+                break;
+
+            default:
+                break;
+        }
             
     }
 
@@ -48,5 +136,23 @@ public class SetTextToTextBox : MonoBehaviour
     {
         Keyboard = 0,
         Gamepad = 1
+    }
+
+    private enum InputActions
+    {
+        Look = 0,
+        Movement = 1,
+        Jump = 2,
+        Sprint = 3,
+        Crouch = 4,
+        Flashlight = 5,
+        DropItem = 6,
+        PickUp = 7,
+        SeeTasks = 8,
+        OpenPager = 9,
+        ConsumeItem = 10,
+        Scroll = 11,
+        OpenBook = 12,
+        StopInspecting = 13
     }
 }
--- Producing Games - Demo/Assets/_Dev/Alex/Scripts/Button Prompts/SwapTextWithSprite.cs
+++ Producing Games - Demo/Assets/_Dev/Alex/Scripts/Button Prompts/SwapTextWithSprite.cs
@@ -9,19 +9,19 @@ public static class SwapTextWithSprite
 {
    public static string ReadAndReplaceBinding(string textToDisplay, InputBinding actionNeeded, TMP_SpriteAsset spriteAsset)
     {
-        string stringButtonName = actionNeeded.ToString();
+        string stringButtonName = actionNeeded.path.ToString();
         stringButtonName = RenameInput(stringButtonName);
 
         textToDisplay = textToDisplay.Replace(
-            oldValue:"BUTTONPROMPT", newValue: $"<sprite=\"{spriteAsset.name}\" name =\"{stringButtonName}\">");
+            oldValue:"BUTTONPROMPT", newValue: $"<sprite=\"{spriteAsset.name}\" name=\"{stringButtonName}\">");
 
         return textToDisplay;
     }
 
     private static string RenameInput(string stringButtonName)
     {
-        stringButtonName = stringButtonName.Replace(
-            oldValue:"Interact:", newValue: String.Empty);
+        //stringButtonName = stringButtonName.Replace(
+            //oldValue:"Interact:", newValue: String.Empty);
 
         stringButtonName = stringButtonName.Replace(
             oldValue: "<Keyboard>/", newValue: "Keyboard_");
